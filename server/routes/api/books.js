74641c7aeeef53cf6dbafb0203df727abe9c0254
Aurel Hudec <hudec117@gmail.com>
2020-06-06 17:52:31 +0100

    Added comments.

--- server/routes/api/books.js
+++ server/routes/api/books.js
@@ -3,21 +3,19 @@ const models = require('../../models');
 const express = require('express');
 const router = express.Router();
 
+// GET /api/books
 router.get('/', (req, res) => {
     models.Book.find({}).then(books => {
         const returnedBooks = books.map(book => book.toClient());
         res.json(returnedBooks);
-    }).catch(err => {
-        console.log(err);
-    });
+    }).catch(console.error);
 });
 
+// GET /api/books/:id
 router.get('/:id', (req, res) => {
     models.Book.findById(req.params.id).then(book => {
         res.json(book.toClient());
-    }).catch(err => {
-        console.log(err);
-    });
+    }).catch(console.error);
 });
 
 module.exports = router;
--- server/routes/users.js
+++ server/routes/users.js
@@ -3,10 +3,12 @@
 const express = require('express');
 const router = express.Router();
 
+// GET /users/login
 router.get('/login', (req, res) => {
     res.json([]);
 });
 
+// POST /users/register
 router.post('/register', (req, res) => {
     res.json([]);
 });
