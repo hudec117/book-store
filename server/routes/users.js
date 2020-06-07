84fbb372616f37ed75c012b396d95cb9a0b0b443
Aurel Hudec <hudec117@gmail.com>
2020-06-07 19:48:00 +0100

    Initial register logic completed.

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,7 +1,11 @@
-// const models = require('../models');
+const models = require('../models');
 
+const validator = require('validator');
+const bcrypt = require('bcrypt');
 const express = require('express');
 const router = express.Router();
+
+const SALT_ROUNDS = 8;
 
 // POST /users/login
 router.post('/login', (req, res) => {
@@ -10,7 +14,67 @@ router.post('/login', (req, res) => {
 
 // POST /users/register
 router.post('/register', (req, res) => {
-    res.json([]);
+    const name = req.body.name;
+    const email = req.body.email;
+    const password = req.body.password;
+
+    // Validate inputs
+    if (validator.isEmpty(name, { ignore_whitespace: true })) {
+        failure(res, 400, 'name_invalid');
+    } else if (!validator.isEmail(email)) {
+        failure(res, 400, 'email_invalid');
+    } else if (validator.isEmpty(password, { ignore_whitespace: true })) {
+        failure(res, 400, 'password_invalid');
+    } else {
+        // Check if user already exists by email
+        models.User.findOne({ email: email }, (err, user) => {
+            if (err != null) {
+                // Failed to query database
+                console.error(err);
+                failure(res, 500, 'server_error');
+            } else if (user != null) {
+                // Existing user found
+                failure(res, 400, 'user_exists');
+            } else {
+                // Existing user not found, generate password hash
+                bcrypt.hash(password, SALT_ROUNDS, (err, hash) => {
+                    if (err != null) {
+                        console.error(err);
+                        failure(res, 500, 'server_error');
+                    } else {
+                        const newUser = new models.User({
+                            name: name,
+                            email: email,
+                            passwordHash: hash
+                        });
+
+                        // Insert new user into database
+                        newUser.save().then(() => {
+                            success(res, 'created');
+                        }).catch((err) => {
+                            console.error(err);
+                            failure(res, 500, 'server_error');
+                        });
+                    }
+                });
+            }
+        });
+    }
 });
+
+function success(response, reason) {
+    response.json({
+        success: true,
+        reason: reason
+    });
+}
+
+function failure(response, code, reason) {
+    response.status(code)
+            .json({
+                success: false,
+                reason: reason
+            });
+}
 
 module.exports = router;
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -6,6 +6,7 @@
                           label-for="name-input">
                 <b-form-input id="name-input"
                               v-model="name"
+                              v-bind:disabled="registering"
                               required
                               placeholder="Enter your name">
                 </b-form-input>
@@ -16,6 +17,7 @@
                           label-for="email-input">
                 <b-form-input id="email-input"
                               v-model="email"
+                              v-bind:disabled="registering"
                               type="email"
                               required
                               placeholder="Enter your email">
@@ -27,6 +29,7 @@
                           label-for="password-input">
                 <b-form-input id="password-input"
                               v-model="password"
+                              v-bind:disabled="registering"
                               type="password"
                               required
                               placeholder="Enter password">
@@ -38,9 +41,10 @@
                           label-for="password-input-2">
                 <b-form-input id="password-input-2"
                               v-model="passwordRetype"
+                              v-bind:disabled="registering"
+                              v-bind:state="passwordsMatch"
                               type="password"
                               required
-                              :state="passwordsMatch"
                               placeholder="Enter password again">
                 </b-form-input>
                 <b-form-invalid-feedback :state="passwordsMatch">
@@ -48,7 +52,12 @@
                 </b-form-invalid-feedback>
             </b-form-group>
 
-            <b-button type="submit" variant="primary">Register</b-button>
+            <b-button v-bind:disabled="registering"
+                      type="submit"
+                      variant="primary">
+                <b-spinner v-if="registering" small></b-spinner>
+                {{ registering ? 'Registering...' : 'Register' }}
+            </b-button>
         </b-form>
     </div>
 </template>
@@ -59,7 +68,8 @@
                 name: '',
                 email: '',
                 password: '',
-                passwordRetype: ''
+                passwordRetype: '',
+                registering: false
             };
         },
         computed: {
@@ -79,15 +89,27 @@
                     password: this.password
                 };
 
+                this.registering = true;
+
                 fetch('/users/register', {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json'
                     },
                     body: JSON.stringify(registerBody)
-                }).then(response => {
-                    console.log(response);
-                }).catch(console.error);
+                }).then(async response => {
+                    const body = await response.json();
+                    if (body.success) {
+                        this.$router.push('books');
+                    } else {
+                        // TODO: handle
+                    }
+                // eslint-disable-next-line no-unused-vars
+                }).catch((err) => {
+                    // TODO: handle
+                }).finally(() => {
+                    this.registering = false;
+                });
             }
         }
     };
