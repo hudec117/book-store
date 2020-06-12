aacd22fc6ac9d83a134d0920aaa72b45da07ada1
Aurel Hudec <hudec117@gmail.com>
2020-06-12 13:53:53 +0100

    Database seeding updated.

--- server/seed-database.js
+++ server/seed-database.js
@@ -21,17 +21,17 @@ models.Book.insertMany(books).then(() => {
     console.log("Seeding completed");
 });
 
-// Generate test users
+// Generate test users, password is "pass" for both.
 const regularUser = new models.User({
     name: 'Myrtle Smith',
     email: 'user@test.com',
-    passwordHash: '$2y$08$lrg/H1E1xadlJPCOxj9.2ejtWKxNciip5XaeRDN5lmqRmTR1QrlBS'
+    passwordHash: '$2b$08$N2w9kWVr5KKbuSRTi7rek.0Z7U55Fs/3BpTWWTBQtMViKFrnPs2mC'
 });
 
 const staffUser = new models.User({
     name: 'Liam Woods',
     email: 'staff@test.com',
-    passwordHash: '$2y$08$lrg/H1E1xadlJPCOxj9.2ejtWKxNciip5XaeRDN5lmqRmTR1QrlBS',
+    passwordHash: '$2b$08$N2w9kWVr5KKbuSRTi7rek.0Z7U55Fs/3BpTWWTBQtMViKFrnPs2mC',
     staff: true
 });
 
