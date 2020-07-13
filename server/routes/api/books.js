71286f746b762b44df622e70a73416edf653adae
Aurel Hudec <hudec117@gmail.com>
2020-07-14 00:26:00 +0100

    Added book creation API.

--- server/routes/api/books.js
+++ server/routes/api/books.js
@@ -19,6 +19,29 @@ router.get('/:id', (req, res) => {
     }).catch(console.error);
 });
 
+// POST /api/books
+router.post('/', jwt({ secret: process.env.JWT_SECRET }), async (req, res) => {
+    // Create new Book object
+    let newBook = new models.Book(req.body);
+
+    // Validate the book
+    try {
+        await newBook.validate();
+    } catch (error) {
+        return failure(res, 400, error.message);
+    }
+
+    // Insert book into database
+    try {
+        newBook = await newBook.save();
+    } catch (error) {
+        console.error(error);
+        return failure(res, 500, 'server_error');
+    }
+
+    res.json(newBook.toClient());
+});
+
 // PUT /api/books/:id
 router.put('/:id', jwt({ secret: process.env.JWT_SECRET }), async (req, res) => {
     if (!req.user.staff) {
