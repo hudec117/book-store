30c60ad4f77efe5c9b5dd301a07732da66653c25
Aurel Hudec <hudec117@gmail.com>
2020-07-21 22:27:23 +0100

    Initial implementation of server-side cover upload.

--- server/routes/api/books.js
+++ server/routes/api/books.js
@@ -1,10 +1,21 @@
 const models = require('../../models');
 
+const { v4: uuidv4 } = require('uuid');
+const path = require('path');
+
+const fileUpload = require('express-fileupload');
 const jwt = require('express-jwt');
 const express = require('express');
 const router = express.Router();
 
-// GET /api/books
+const COVERS_FOLDER = 'covers';
+
+const MIME_TO_EXT_MAP = {
+    'image/jpeg': '.jpg',
+    'image/png': '.png'
+};
+
+// GET /api/books (get all books)
 router.get('/', (req, res) => {
     models.Book.find({}).then(books => {
         const booksToReturn = books.map(book => book.toClient());
@@ -12,17 +23,18 @@ router.get('/', (req, res) => {
     }).catch(console.error);
 });
 
-// GET /api/books/:id
+// GET /api/books/:id (get specific book)
 router.get('/:id', (req, res) => {
     models.Book.findById(req.params.id).then(book => {
         res.json(book.toClient());
     }).catch(console.error);
 });
 
-// POST /api/books
-router.post('/', jwt({ secret: process.env.JWT_SECRET }), async (req, res) => {
+// POST /api/books (create book)
+router.post('/', fileUpload(), jwt({ secret: process.env.JWT_SECRET }), async (req, res) => {
     // Create new Book object
-    let newBook = new models.Book(req.body);
+    const bookObj = JSON.parse(req.body.book);
+    let newBook = new models.Book(bookObj);
 
     // Validate the book
     try {
@@ -31,7 +43,30 @@ router.post('/', jwt({ secret: process.env.JWT_SECRET }), async (req, res) => {
         return failure(res, 400, error.message);
     }
 
-    // Insert book into database
+    // Validate the covers
+    const covers = Array.isArray(req.files) ? req.files.covers : [req.files.covers];
+    if (covers.length < 1) {
+        return failure(res, 400, 'invalid_covers');
+    }
+
+    // Attempt to upload covers
+    try {
+        for (const cover of covers) {
+            const mimeType = cover.mimetype;
+            const fileName = `${uuidv4()}${MIME_TO_EXT_MAP[mimeType]}`;
+
+            const clientPath = `${COVERS_FOLDER}/${fileName}`;
+            const serverPath = path.join('public', COVERS_FOLDER, fileName);
+
+            await cover.mv(serverPath);
+            newBook.covers.push(clientPath);
+        }
+    } catch (error) {
+        console.error(error);
+        return failure(res, 500, 'server_upload_error');
+    }
+
+    // Insert book
     try {
         newBook = await newBook.save();
     } catch (error) {
@@ -39,10 +74,10 @@ router.post('/', jwt({ secret: process.env.JWT_SECRET }), async (req, res) => {
         return failure(res, 500, 'server_error');
     }
 
-    res.json(newBook.toClient());
+    return res.json(newBook.toClient());
 });
 
-// PUT /api/books/:id
+// PUT /api/books/:id (update book)
 router.put('/:id', jwt({ secret: process.env.JWT_SECRET }), async (req, res) => {
     if (!req.user.staff) {
         return failure(res, 401, 'unauthorised');
